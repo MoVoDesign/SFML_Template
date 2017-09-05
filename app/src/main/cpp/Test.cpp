ccea6b86235c200aab858f2a132cc4f220eb652b
lio <chezlio@yahoo.com>
2017-09-05 20:49:16 +0200

    v1.2

---
+++ app/src/main/cpp/Test.cpp
@@ -0,0 +1,5 @@
+//
+// Created by LIO on 05/09/2017.
+//
+
+#include "Test.h"
---
+++ app/src/main/cpp/Test.h
@@ -0,0 +1,16 @@
+//
+// Created by LIO on 05/09/2017.
+//
+
+#ifndef SFML_TEMPLATE_TEST_H
+#define SFML_TEMPLATE_TEST_H
+
+
+
+class Test {
+
+};
+
+
+
+#endif //SFML_TEMPLATE_TEST_H
